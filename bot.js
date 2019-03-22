7de96a10a625033df3a34e10c3f94072e394aafa
siwar <siwar.madrane@gmail.com>
2019-03-22 16:02:32 +0100

    Testing sendActivity changes

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ class EchoBot {
             // read from state.
             let count = await this.countProperty.get(turnContext);
             count = count === undefined ? 1 : ++count;
-            await turnContext.sendActivity(`${ count }: You said "${ turnContext.activity.text }"`);
+            await turnContext.sendActivity(`${ count }: Panora.Me You said "${ turnContext.activity.text }"`);
             // increment and set turn counter.
             await this.countProperty.set(turnContext, count);
         } else {
